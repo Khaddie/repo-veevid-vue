5c14a872cfe9fc1ecfc5993b07686301b073e1e5
Camelia <camelia.siong@edu.univ-fcomte.fr>
2022-01-07 15:11:48 +0100

    Ajout des différentes pages du site

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,46 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<!-- VALEURS PAGE A PROPOS -->
+<section class="c-valeurs">
+    <div class="row">
+        <div class="col-12 col-md-6 c-valeurs__col">
+            <div class="c-valeur">
+                <div class="c-valeur__content">
+                    <h3 class="c-valeur__title">TRANSPARENCE</h3>
+                    <p class="c-valeur__text">Veevid tient à rester transparent sur la provenance de tous ses produits. La marque veut permettre à ses clients d’acheter en toute conscience en respectant leur éthique et leurs valeurs. Nos chaussures sont toutes produites en Europe et leurs composants sont importés d’Asie dans le respect du commerce équitable. </p>
+                </div> 
+            </div>
+            <div class="c-valeur">
+                <div class="c-valeur__content">
+                    <h3 class="c-valeur__title">TRANSPARENCE</h3>
+                    <p class="c-valeur__text">Veevid tient à rester transparent sur la provenance de tous ses produits. La marque veut permettre à ses clients d’acheter en toute conscience en respectant leur éthique et leurs valeurs. Nos chaussures sont toutes produites en Europe et leurs composants sont importés d’Asie dans le respect du commerce équitable. </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 c-valeurs__col">
+            <div class="c-valeur">
+                <div class="c-valeur__content">
+                    <h3 class="c-valeur__title">TRANSPARENCE</h3>
+                    <p class="c-valeur__text">Veevid tient à rester transparent sur la provenance de tous ses produits. La marque veut permettre à ses clients d’acheter en toute conscience en respectant leur éthique et leurs valeurs. Nos chaussures sont toutes produites en Europe et leurs composants sont importés d’Asie dans le respect du commerce équitable. </p>
+                </div>
+            </div>
+            <div class="c-valeur">
+                <div class="c-valeur__content">
+                    <h3 class="c-valeur__title">TRANSPARENCE</h3>
+                    <p class="c-valeur__text">Veevid tient à rester transparent sur la provenance de tous ses produits. La marque veut permettre à ses clients d’acheter en toute conscience en respectant leur éthique et leurs valeurs. Nos chaussures sont toutes produites en Europe et leurs composants sont importés d’Asie dans le respect du commerce équitable. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- FIN VALEURS PAGE A PROPOS -->
 </template>
+
+<script>
+export default {
+    name: "About"
+}
+</script>
+
+<style lang="scss">
+ 
+</style>  
---
+++ src/views/Concours.vue
@@ -0,0 +1,35 @@
+<template>
+    <!-- BANDEAU CONCOURS -->
+
+<div class="row -center-step">
+    <div class="col-12 col-md-4 col-l-3">
+        <div class="c-step">
+            <div class="c-step__number">1</div>
+            <p class="c-step__text">S’abonner au compte instagram officiel @veevid⁠</p>
+        </div>
+    </div>
+    <div class="col-12 col-md-4 col-l-3">
+        <div class="c-step">
+            <div class="c-step__number">2</div>
+            <p class="c-step__text">Ajouter le #VeevidCustom sous ta publication</p>
+        </div>
+    </div>
+    <div class="col-12 col-md-4 col-l-3">
+        <div class="c-step">
+            <div class="c-step__number">3</div>
+            <p class="c-step__text">Poster ta paire sur Instagram</p>
+        </div>
+    </div>
+</div>
+    <!-- FIN BANDEAU CONCOURS -->
+</template>
+
+<script>
+export default {
+    name: "Concours"
+}
+</script>
+
+<style lang="scss">
+ 
+</style>
