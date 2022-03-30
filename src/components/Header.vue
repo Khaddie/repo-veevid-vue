b021c64b0a97162a76a3ba25ac912ddc3fe92059
Khaddie <khaddie90@gmail.com>
2022-03-30 15:42:33 +0200

    menukadi

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,73 @@
 <template>
 <div>
 
+  <div class="menurespon" ref="root">
+    <div>
+      <div class="logo">
+        <router-link to="/">
+          <img src="@/assets/logo/Logo_base.svg" alt="Logo Khadija Boudjemline">
+        </router-link>
+      </div>
+    </div>
+    <nav>
+      <button class="menubtn" v-on:click="menuclick">
+      <span>
+      </span>
+      </button>
+      <div class="menuo" style="display: none" ref="menu" v-on:click="menuclick">
+        <div class="image">
+          <div data-project-no="1" class="item item--1 w-100" style=" opacity: 1; z-index: 89;">
+            <img class="imgmenu w-100" src="@/assets/logo/Logo_base.svg" alt="Portfolio Khadija Boudjemline">
+          </div>
+        </div>
+        <div class="list">
+          <ul v-on:click="fermeture">
+            <router-link to="/Logofolio">
+              <li>Logofolio</li>
+            </router-link>
+            <router-link to="/Sitesweb">
+              <li ref="fermli">Sites Web</li>
+            </router-link>
+            <router-link to="/Creations">
+              <li>Créations graphiques</li>
+            </router-link>
+            <router-link to="/Apropos">
+              <li>À propos</li>
+            </router-link>
+
+            <router-link to="/Contact">
+              <li>Contact</li>
+            </router-link>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+
+
+   <!--<div class="menumob">
+     <div class="menubtn" v-on:click="menuclick" >
+       <img src="@/assets/icons/menu.png" alt="" />
+     </div>
+     <div class="menubtn" v-on:click="menuclick" >
+       <img src="@/assets/logo/Logo_base.svg" alt="" />
+     </div>
+   </div>
+  </div>
+  <div class="listmobile" ref="menu" v-on:click="menuclick">
+
+    <div>
+      <ul class="listmenu" v-on:click="fermeture">
+        <li>Vee One Nawaka</li>
+
+        <li>Il était une fois Veevid</li>
+
+        <li>Jeu concours</li>
+
+        <li>Contactez-nous</li>
+      </ul>
+    </div>
+  </div>-->
   <header class="header" id="header">
     <nav class="navbar container">
       <router-link to="/">
@@ -100,19 +167,7 @@
       </nav>
     </header>-->
 
-  <div class="listmobile" ref="menu" v-on:click="menuclick">
-    <div>
-      <ul class="listmenu" v-on:click="fermeture">
-        <li>Vee One Nawaka</li>
 
-        <li>Il était une fois Veevid</li>
-
-        <li>Jeu concours</li>
-
-        <li>Contactez-nous</li>
-      </ul>
-    </div>
-  </div>
 </div>
 </template>
 
@@ -134,7 +189,7 @@ export default {
       }
       },
   fermeture: function (fermer) {
-    this.$refs["menu"].style.display = 'none';
+    this.$refs["menup"].style.display = 'none';
 
   },
 }
@@ -143,6 +198,14 @@ export default {
 <style lang="scss">
 .imgicon{
   width: 45%;
+}
+
+.menumob{
+  display: flex;
+  flex-direction: row-reverse;
+  img{
+    width: 100%;
+  }
 }
 #header{
   background: no-repeat url("~@/assets/draw/forme_header.svg");
@@ -154,7 +217,7 @@ export default {
 
 
   @include small-down(){
-    display: none;
+display: none;
   }
 }
 // Elements
@@ -304,44 +367,186 @@ a{
 }
 
 
-.listmobile{
 
-  display: none;
 
-  @include small-down(){
-    justify-content: center;
-    line-height: 5rem;
+.menu-open nav::before {
+  transform: scale(50);
+}
+.menuo ul {
+  list-style: none;
+  color: #DBBCA8;
+
+}
+
+.menurespon{
+
+  .image, .list {
+    height: 100%;
+  }
+
+  .image {
+    position: relative;
+  }
+
+  .item {
     position: absolute;
-    text-align: center;
-    margin-top: 10%;
-    margin-left: 25%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-size: cover;
+    background-position: center;
+    opacity: 0;
   }
 
-}
-
-.menubtn {
-
-  img {
-    width: 1.5rem;
-    float: right;
-    margin-left: 110px;
+  .list {
+    margin-top: 20%;
+    padding: 20px;
   }
 
-  @include large-up() {
+
+  a{
+    text-decoration: none;
+    color: black;
+  }
+  a:hover{
+    text-decoration: none;
+    color: black;
+  }
+  .menuo a:hover {
+    opacity: .4;
+    text-decoration: none;
+    color: #DBBCA8;
+  }
+
+
+  /**FIN*/
+  nav {
+
+    width: 70px;
+    height: 70px;
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 10;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+  }
+
+  nav::before {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    display: block;
+    background: white;
+    border-radius: 100%;
+    transition: transform 400ms ease;
+    z-index: 10;
+
+  }
+
+  nav::after {
+    content: '';
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: block;
+    background: transparent;
+    border-radius: 50%;
+    transition: transform 400ms ease-in;
+    z-index: 40;
+  }
+
+  nav:hover::after {
+    transform: scale(1.2);
+  }
+
+  .menubtn {
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    z-index: 140;
+    border: none;
+    top: 17px;
+    left: 14px;
+    border-radius: 50px;
+    box-shadow: #DBBCA8;
+    background-image: url("~@/assets/icons/ico-menu.svg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 70%;
+    background-color: transparent;
+  }
+
+  .menubtn::before {
+    content: '';
+    border-radius: 50%;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+
+
+  }
+
+
+
+  .menuo {
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background: white;
+    z-index: 20;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+
+
+  .image {
+    display: none;
+  }
+
+  .list {
+    margin-top: 40%;
+  }
+
+  .menuo li {
+    font-size: 20px;
+    margin-bottom: 30px;
+    font-family: Lato, sans-serif;
+    font-weight: 300;
+  }
+
+  .menurespon a {
+    transition: opacity 300ms ease;
+    width: fit-content;
+    font-size: 30px;
+    margin-bottom: 10px;
+    text-decoration: none;
+    color: black;
+  }
+
+  .logo img{
+    width: 25%;
+    margin: 13% 5%;
+  }
+
+
   display: none;
+  @include small-down(){
+    display: block;
   }
-
-  }
-.menu-open{
-
-  background-color: #fff;
-  width: 100%;
-  height: 170%;
-  margin-bottom: 50%;
 }
-
-body {
-  margin-top: 0;
+#app > div:nth-child(2) > section.c-concoursaccueil{
+  display: none;
 }
-
 </style>
